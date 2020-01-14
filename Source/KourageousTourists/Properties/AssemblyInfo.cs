d9100f4c7c69e644889dddc9ac046c53ea573ea3
Lisias <me@lisias.net>
2020-01-14 20:17:40 -0300

    Versioning Normalizing

--- Source/KourageousTourists/Properties/AssemblyInfo.cs
+++ Source/KourageousTourists/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle ("KourageousTourists")]
+[assembly: AssemblyTitle ("KourageousTourists /L Unofficial")]
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("nmakeev")]
+[assembly: AssemblyCopyright ("© 2017 nmakeev, © 2020 LisiasT")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("1.0.*")]
+[assembly: AssemblyVersion (KourageousTourists.Version.Number)]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
